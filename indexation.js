a45bf45256e4f8dde3f37aa5068c9193e34ec9bf
notkojak <torrespierre@outlook.com>
2025-11-10 21:33:33 +0100

    first commit

--- indexation.js
+++ indexation.js
@@ -1,13 +1,9 @@
-import { index } from 'google-indexing-script'
-import serviceAccount from './i1.json' with { type: 'json' } // À remplacer par le chemin de votre fichier JSON
+import { index } from "google-indexing-script";
+import serviceAccount from "./i1.json" with { type: "json" };
 
-
-index('marineblog.net', {
+index("marineblog.net", {
   client_email: serviceAccount.client_email,
   private_key: serviceAccount.private_key,
-  quota: {
-    rpmRetry: true
-  }
 })
   .then(console.log)
-  .catch(console.error)
+  .catch(console.error);
